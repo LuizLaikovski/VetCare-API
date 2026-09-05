9d5410b7bde770eefe4ff0c421a5a684f0574293
Luiz Henrique Pasin Laikovski <160736729+LuizLaikovski@users.noreply.github.com>
2026-09-05 12:09:47 -0300

    arrumando a rota de not found e tratameto dessa exceção

--- src/main/java/com/vetcare/petmeds/exception/GlobalExceptionHandler.java
+++ src/main/java/com/vetcare/petmeds/exception/GlobalExceptionHandler.java
@@ -1,26 +1,78 @@
 package com.vetcare.petmeds.exception;
 
 import com.vetcare.petmeds.shared.ErrorDTO;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorDTO> handleResourceNotFound(ResourceNotFoundException ex) {
-        return new ResponseEntity<>(new ErrorDTO(ex.getMessage()), HttpStatus.NOT_FOUND);
-    }
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(
+        NoHandlerFoundException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        ErrorDTO errorDTO = new ErrorDTO(
+            new Date(),
+            "A rota inserida é inválida!",
+            request.getDescription(false)
+        );
+
+         return new ResponseEntity<>(errorDTO, HttpStatus.NOT_FOUND);
+     }
 
     @ExceptionHandler(UnauthorizedException.class)
-    public ResponseEntity<ErrorDTO> handleUnauthorized(UnauthorizedException ex) {
-        return new ResponseEntity<>(new ErrorDTO(ex.getMessage()), HttpStatus.UNAUTHORIZED);
+    public ResponseEntity<ErrorDTO> handleUnauthorized(UnauthorizedException ex, WebRequest request) {
+        ErrorDTO errorDTO = new ErrorDTO(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+        return new ResponseEntity<>(errorDTO, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorDTO> handleGeneralException(Exception ex) {
-        return new ResponseEntity<>(new ErrorDTO("Erro interno no servidor: " + ex.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+    public final ResponseEntity<ErrorDTO> handleGeneralException(Exception ex, WebRequest request) {
+        ErrorDTO errorDTO = new ErrorDTO(
+                new Date(),
+                "Erro interno do servidor!",
+                request.getDescription(false)
+        );
+        return new ResponseEntity<>(errorDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public final ResponseEntity<ErrorDTO> handleBadRequest(BadRequestException ex, WebRequest request) {
+        ErrorDTO errorDTO = new ErrorDTO(
+                new Date(),
+                "Rota não encontrada",
+                request.getDescription(false)
+        );
+
+        return new ResponseEntity<>(errorDTO, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public final ResponseEntity<ErrorDTO> handleTypeMismatch(MethodArgumentTypeMismatchException ex, WebRequest request) {
+        ErrorDTO errorDTO = new ErrorDTO(
+                new Date(),
+                "Parâmetro inválido: " + ex.getName() + " deve ser do tipo " + ex.getRequiredType().getSimpleName(),
+                request.getDescription(false)
+        );
+
+        return new ResponseEntity<>(errorDTO, HttpStatus.BAD_REQUEST);
     }
 }
+
--- src/main/java/com/vetcare/petmeds/shared/ErrorDTO.java
+++ src/main/java/com/vetcare/petmeds/shared/ErrorDTO.java
@@ -1,10 +1,18 @@
 package com.vetcare.petmeds.shared;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class ErrorDTO {
+    @JsonFormat(shape =  JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date timestamp;
     private String message;
+    private String description;
 }
